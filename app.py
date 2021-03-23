56382a05fb3a8372d8c5e1ca5e9c49cee6f5ac55
db0g <damienb.dev@outlook.fr>
2021-03-23 21:32:42 +0100

    responsivite

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('pages/home.html')
+def index():
+    return render_template('pages/index.html')
 
 if (__name__) == '__main__':
     app.run(debug=True)
