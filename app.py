a9c83f50137d5269485a2faa5046274752196927
db0g <damienb.dev@outlook.fr>
2021-03-24 16:35:26 +0100

    bootstrap

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('pages/index.html')
+def col_raw_border():
+    return render_template('pages/col_row_border.html')
+
+@app.route('/width_height')
+def width_height():
+    return render_template('pages/width_height.html')
+
+@app.route('/alert_modal')
+def alert_modal():
+    return render_template('pages/alert_modal.html')
 
 if (__name__) == '__main__':
     app.run(debug=True)
